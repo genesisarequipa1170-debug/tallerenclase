5867b06c2235798a13437b1be0335eb3468417fe
unknown <genesis.arequipa1170@utc.edu.ec>
2026-06-05 02:26:59 -0500

    cambios genesis 4

--- app/src/main/java/com/example/TALEER/App.java
+++ app/src/main/java/com/example/TALEER/App.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        // Ingresar variables
         int numero;
         int positivos = 0;
         int negativos = 0;
