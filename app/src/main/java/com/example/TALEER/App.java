54e23f5acc33624a9b2a8c3a4711086de7a687da
unknown <genesis.arequipa1170@utc.edu.ec>
2026-06-05 02:02:40 -0500

    comit gene

--- app/src/main/java/com/example/TALEER/App.java
+++ app/src/main/java/com/example/TALEER/App.java
@@ -14,5 +14,6 @@ public class App {
         System.out.println("chen");
         System.out.println("issac gay");
         System.out.println("genesis");
+        System.out.println("quiero dormir ");
     }
 }
